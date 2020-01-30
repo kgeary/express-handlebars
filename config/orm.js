12be77681d5e82455a68a58b61e5b24e8730440d
Kevin Geary <kgeary@gmail.com>
2020-01-29 20:00:06 -0600

    Changed to parametized values for queries. Updated readme and screenshot

--- config/orm.js
+++ config/orm.js
@@ -1,12 +1,12 @@
 const connection = require("./connection");
 
-function query(sql) {
+function query(sql, values) {
   return new Promise((resolve, reject) => {
-    connection.query(sql, (err, res) => {
+    connection.query(sql, values, (err, res) => {
       if (err) return reject(err);
       resolve(res);
+    })
   })
-})
 }
 
 function selectAll() {
@@ -16,14 +16,16 @@ function selectAll() {
 function insertOne(burger) {
   return query(
     `INSERT INTO burgers (burger_name, devoured) 
-    VALUES ("${burger.name}", ${burger.devoured})`);
+    VALUES (?,?)`,
+    [burger.name, burger.devoured]);
 }
 
 function updateOne(burger) {
   return query(
     `UPDATE burgers 
-    SET devoured=${burger.devoured} 
-    WHERE id=${burger.id}`);
+    SET devoured=? 
+    WHERE id=?`,
+    [burger.devoured, burger.id]);
 }
 
 module.exports = {
--- docs/README.md
+++ docs/README.md
@@ -1,2 +1,11 @@
 # express-handlebars
-A burger logger using Node, Express, Handlebars, MySQL
+A logging app using Node, Express, Handlebars, MySQL  
+This project is an example of server side rendering.
+* Dynamically renders pages using Handlebars
+* Serves content using express 
+* Tracks data with MySQL database
+* Minimal client-side scripting.
+
+## Live Site
+[Burger App @ Heroku](https://express-burger-app.herokuapp.com/)
+<img src="screenshot.png" alt="Screenshot of Burger App Site in Action">
