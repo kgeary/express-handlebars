85fd84f95cca1f4b969e6c203b5d2ed736218359
Kevin Geary <kgeary@gmail.com>
2020-01-29 18:47:13 -0600

    Changed the port to an ENV

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
+    port: process.env.PORT || 3306,
     user: "root",
     password: process.env.DB_PASSWORD,
     database: "burger_db"
