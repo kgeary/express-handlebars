b83bf678345744ca908668d4e447f557439e2592
Kevin Geary <kgeary@gmail.com>
2020-01-29 18:14:46 -0600

    Updated connection to check for JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 require("dotenv").config();
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.DB_PASSWORD,
-  database: "burger_db"
-});
+if (process.env.JAWSDB_URL) {
+  const connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.DB_PASSWORD,
+    database: "burger_db"
+  });
+}
 
 module.exports = connection;
